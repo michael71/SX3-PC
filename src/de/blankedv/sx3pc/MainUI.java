f1f6b25d487ded29dbee3ddd15935be42ee6cf1b
mblank <mblank@bawue.de>
2018-08-09 14:40:16 +0200

    updated config server

--- src/de/blankedv/sx3pc/MainUI.java
+++ src/de/blankedv/sx3pc/MainUI.java
@@ -39,8 +39,8 @@ public class MainUI extends javax.swing.JFrame {
     /**
      * {@value #VERSION} = program version, displayed in HELP window
      */
-    public static final String VERSION = "2.30 - 04 Aug 2018";
-    public static final String S_XNET_SERVER_REV = "SXnet-Server 3.1 - 04 Aug 2018";
+    public static final String VERSION = "2.31 - 09 Aug 2018";
+    public static final String S_XNET_SERVER_REV = "SXnet-Server 3.1 - SX3PC - 09 Aug 2018";
 
     /**
      * {@value #SX_MIN} = minimale SX adresse angezeigt im Monitor
